9bcca710709a63d7b6f9ba3132538ac1545c8c4f
NelsonEAX <nelsoneax@yandex.ru>
2018-12-13 06:26:18 +0500

    Add firebase config

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueFire)
+firebase.initializeApp({
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    databaseURL: process.env.VUE_APP_DB_URL
+})
+
 Vue.config.productionTip = false
 
 new Vue({
